4334214692b65396dceea9554c38ba4bfb9647ef
JoaoPdSsilva <60629168+JoaoPdSsilva@users.noreply.github.com>
2021-10-08 14:39:24 +0000

    11:39 (terminar // video 17:03)

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,4 +1,5 @@
-blank = "A"
+branco = "  "
+token = ["X", "O"]
 
 
 def criarBoard():
@@ -14,25 +15,45 @@ def criarBoard():
 def printBoard(board):
     for i in range(3):
         print("|".join(board[i]))
-        print("------")
+        if (i < 2):
+            print("------")
 
 
 def getInputValido(mensagem):
-    print(mensagem)
     try:
         n = int(input(mensagem))
-        print("Sucesso", n)
+        if (n >= 1 and n <= 3):
+            return n - 1
+        else:
+            print("Numero precisa estar entre 1 e 3")
+            return getInputValido(mensagem)
     except:
-               print("Numero não valido")
-               getInputValido(mensagem)
-
-def verificaMovimento():
-    pass
+        print("Numero não valido")
+        return getInputValido(mensagem)
 
 
-def fazMovimento():
-    pass
+def verificaMovimento(i, j, board):
+    if (board[i][j] == blank):
+        return True
+    else:
+        return False
+
+
+def fazMovimento(i, j, board, jogador):
+    board[i][j] = token[jogador]
 
 
 def verificaGanhador(board):
+    #linhas
+    for i in range(3):
+      if(board[i][[0] == board[i][1] and board[i][1] == board[i][2]] and board[i][0] != branco):
+        return board[i][0]
+ #colunas
+  for i in range(3):
+    if(board[0][[i] == board[1][i] and board[1][i] == board[2][i]] and board[0][i] != branco):
+        return board[0][i]
+
+  
+  if(board[0[0] != branco and board[0][0] == board[1][1] ==[])
+
     return False
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from jogo_da_velha import criarBoard, fazMovimento, getInputValido, printBoard, verificaGanhador, verificaMovimento
 
-jogador = 0 #jogador 1 
-
+jogador = 0  #jogador 1
 
 board = criarBoard()
 print(board)
 ganhador = verificaGanhador(board)
 while (not ganhador):
     printBoard(board)
-    getInputValido("Digite a linha ")
-    break
+    i = getInputValido("Digite a Linha ")
+    j = getInputValido("Digite a coluna")
+
+    if (verificaMovimento(i, j, board)):
+        fazMovimento(i, j, board, jogador)
+        jogador = (jogador + 1) % 2
+    else:
+        print("A posição ta ocupada")
+    ganhador = verificaGanhador(board)
+
+print(ganhador)
